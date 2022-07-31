29f2013d6575447c66687a6ea75a9e71784f3b1c
Iannrst <iannrst11@gmail.com>
2022-07-31 18:37:47 -0300

    App

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import { Header } from "./components/Header"
+
+import './global.css';
+import styles from './App.module.css';
+import { Form } from "./components/Form";
+
+function App() {
+
+
+  return (
+    <>
+      <Header />
+      <Form />
+    </>
+  )
+}
+
+export default App
